71896f17834025e91057ee58608bb274c81b6c4d
nana.user <you1@example.com>
2026-05-21 15:11:38 +0900

    up

--- ShopSample/Program.cs
+++ ShopSample/Program.cs
@@ -43,7 +43,7 @@ builder.Services.ConfigureApplicationCookie(options =>
 {
     options.LoginPath = "/Account/Login";
     options.LogoutPath = "/Account/Logout";
-    options.AccessDeniedPath = "/Account/AccessDenied";
+    options.AccessDeniedPath = "/Account/Login";
 });
 
 var app = builder.Build();
